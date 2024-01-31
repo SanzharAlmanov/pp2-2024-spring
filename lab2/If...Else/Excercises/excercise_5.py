b1db4aa8a4940bf49d9aa5c1f3d532a8028b0fb2
SanzharAlmanov <s_almanov@kbtu.kz>
2024-02-01 00:51:33 +0600

    second lab

---
+++ lab2/If...Else/Excercises/excercise_5.py
@@ -0,0 +1,6 @@
+a = 5
+b = 7
+c = 6
+d = 6
+if a == b and c == d:
+  print("Hello")
---
+++ lab2/Tuples/Unpack tuples/example_1.py
@@ -0,0 +1,2 @@
+#When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+fruits = ("apple", "banana", "cherry")
